5d24929e97aade95ccd64cc98cec3b1726969332
Artem Mialo <timredrick1@gmail.com>
2021-07-29 15:30:34 +0300

    add form to  convert UAH for other currencies

--- src/components/calcBuy/calcBuy.js
+++ src/components/calcBuy/calcBuy.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CalcBuy = (props) => {
-
     
+    const [name, setName] = useState("");
+  
+      const handleSubmit = (evt) => {
+      evt.preventDefault();  
+      }
     return (
         <div className = "CalcBuy">
-            <p> You can buy for: you don't have money</p>
+            <p> You can buy for:</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Amount:
+                    <input
+                        type="number"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    /> UAH
+                </label>
+             </form>
             <ul>
             {props.rates.map((item) => (
-                <li key={item.ccy}>
-                {item.ccy} : {item.sale}
-                </li>
+                <p key={item.ccy}>
+                {item.ccy} : {( name / item.sale  ).toFixed(2)}
+                </p>
             ))}
-            </ul>            
+            </ul>     
+             
         </div>  
+        
     );
 }
 
